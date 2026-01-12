49006e0301c22fbba7e6e477c96af155ac9b7f20
Emilson Filho <92304613+emilsonfilho@users.noreply.github.com>
2026-01-12 20:55:36 +0000

    feat: equipment routes

--- app/main.py
+++ app/main.py
@@ -1,16 +1,8 @@
 from fastapi import FastAPI, Depends
-from routes import auth
+from routes import auth, equipments
 from dependencies import get_db_connection, get_current_user
 
 app = FastAPI()
 
 app.include_router(auth.auth_router, prefix="/auth")
-
-@app.get("/users")
-def get_users(
-    conn = Depends(get_db_connection),
-    current_user: dict = Depends(get_current_user)):
-    return {
-        "user_id": current_user.id,
-        "email": current_user.email
-    }
+app.include_router(equipments.router, prefix="/equipments")
