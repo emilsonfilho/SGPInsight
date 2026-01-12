0902232a7e450fe32f0b0e962ad455064905c2b0
Emilson Filho <92304613+emilsonfilho@users.noreply.github.com>
2026-01-12 20:55:16 +0000

    feat: equipment repository

---
+++ app/repositories/equipment_repository.py
@@ -0,0 +1,37 @@
+from enums import DepartmentEnum, EquipmentStatusEnum
+from psycopg2.extras import RealDictCursor
+
+class EquipmentRepository:
+    def __init__(self, connection):
+        self.conn = connection
+    
+    def get(
+        self,
+        ean: str | None = None,
+        department: DepartmentEnum | None = None,
+        status: EquipmentStatusEnum | None = None
+    ):
+        cursor = self.conn.cursor(cursor_factory=RealDictCursor)
+
+        sql = "SELECT * FROM equipments"
+        conditions = []
+        params = []
+
+        if ean:
+            conditions.append("ean = %s")
+            params.append(ean)
+
+        if department:
+            conditions.append("department_id = %s")
+            params.append(department)
+
+        if status:
+            conditions.append("equipment_status_id = %s")
+            params.append(status)
+
+        if conditions:
+            sql += " WHERE " + " AND ".join(conditions)
+
+        cursor.execute(sql, params)
+
+        return cursor.fetchall()
