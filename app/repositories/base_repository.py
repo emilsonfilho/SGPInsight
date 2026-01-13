564e9d4f9d833932e785b2f3defb89538c715120
Emilson Filho <92304613+emilsonfilho@users.noreply.github.com>
2026-01-13 00:36:59 +0000

    feat: update sql in base repository

--- app/repositories/base_repository.py
+++ app/repositories/base_repository.py
@@ -7,4 +7,12 @@ class BaseRepository:
         placeholders = ', '.join(['%s'] * len(columns))
 
         return cols, placeholders, vals
+
+    def _prepare_update(self, data: dict):
+        set_clauses = [f"{key} = %s" for key in list(data.keys())]
+        set_str = ', '.join(set_clauses)
+
+        vals = list(data.values())
+
+        return set_str, vals
         
