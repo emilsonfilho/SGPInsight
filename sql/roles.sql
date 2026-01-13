7f8b97eb8c0607a9127df7d187ac98d9a6c9ca55
Emilson Filho <92304613+emilsonfilho@users.noreply.github.com>
2026-01-12 22:35:17 -0300

    Delete sql directory

--- sql/roles.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO roles (name) VALUES 
-('2f87ad2e-1fd2-49bb-a2a9-789841d64de5', 'Gerente'),      -- Nível de gestão, responsável por aprovações e relatórios
-('22a544a1-1493-4509-885d-b16745509b2e', 'Analista'),     -- Nível operacional e técnico, responsável pela execução de manutenções e suporte
-('c4a0674c-7643-4737-a274-9955c52aedd5', 'Estagiário');   -- Nível de apoio, com acesso limitado para tarefas básicas
